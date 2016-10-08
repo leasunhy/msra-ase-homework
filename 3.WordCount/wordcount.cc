78e0972f5df001231216c97709c8fd4f048b076f
leasunhy <leasunhy@gmail.com>
2016-10-08 14:00:56 +0800

    3.WordCount: deprecate regex approach in favor of hand-crafted tokenizer

--- 3.WordCount/wordcount.cc
+++ 3.WordCount/wordcount.cc
@@ -1,15 +1,28 @@
 #include <cassert>
+#include <cctype>
 #include <iostream>
 #include <unordered_map>
 #include <string>
 #include <iterator>
 #include <algorithm>
 #include <utility>
-#include <regex>
 #include <thread>
 #include <mutex>
 #include <queue>
 #include <boost/filesystem.hpp>
+
+// XXX(leasunhy): deprecate use of regex in favor of hand-crafted tokenizer.
+#if 0
+// on windows, boost.regex is much faster than the STL one.
+//    However on Linux, the STL one is much faster.
+#ifdef WIN32
+#include <boost/regex.hpp>
+namespace re = boost;
+#else
+#include <regex>
+namespace re = std;
+#endif  // WIN32
+#endif
 
 using std::string;
 using std::pair;
@@ -19,10 +32,12 @@ using std::cout;
 
 namespace fs = boost::filesystem;
 
+using occurences_t = std::unordered_map<string, int>;
+
 std::queue<fs::path> filenames;
 std::mutex filenames_lock;
 
-std::unordered_map<string, int> total_occurences;
+occurences_t total_occurences;
 std::mutex total_occurences_lock;
 
 void worker_func(int id);
@@ -87,14 +102,52 @@ int main(int argc, char * argv[])
     return 0;
 }
 
+bool isw(char c)
+{
+    return std::isalnum(c) || c == '_' || c == '\''; 
+}
+
+string tolower_inplace(string&& str)
+{
+    for (char & c : str)
+        if (c >= 'A' && c <= 'Z')
+            c += 'a' - 'A';
+    return str;
+}
+
+void count_occurences(string& content, occurences_t& occurences)
+{
+    // XXX(leasunhy): deprecate regex approach.
+    //static re::regex expression("\\w[\\w_']*", re::regex_constants::optimize);
+
+    //string::const_iterator beginiter = content.begin(), enditer = content.end();
+    //re::smatch what;
+    //auto flags = re::regex_constants::match_default;
+    //while (re::regex_search(beginiter, enditer, what, expression, flags)) {
+    //    beginiter = what[0].second;
+    //    occurences[what[0]] += 1;
+    //}
+
+    for (size_t i = 0; i < content.length(); ++i) {
+        // a word must start from an alphabetica: \w[\w_']+
+        if (std::isalpha(content[i])) {
+            // search forward
+            size_t j = i + 1;
+            while (isw(content[j]))
+                ++j;
+            occurences[tolower_inplace(content.substr(i, j - i))] += 1;
+            i = j - 1;
+        }
+    }
+}
+
 void worker_func(int id)
 {
 #ifdef DEBUG
     cerr << "Thread #" << id << ": started." << endl;
 #endif  // DEBUG
 
-    std::regex re("\\w+");
-    std::unordered_map<string, int> occurences;
+    occurences_t occurences;
     while (true) {
         filenames_lock.lock();
         if (filenames.empty()) {
@@ -115,13 +168,8 @@ void worker_func(int id)
         //                 since we know that the files are small.
         //                 Improve it.
         string content(std::istreambuf_iterator<char>(fin), {});
-        string::const_iterator beginiter = content.begin(), enditer = content.end();
-        std::smatch what;
-        auto flags = std::regex_constants::match_default;
-        while (regex_search(beginiter, enditer, what, re, flags)) {
-            beginiter = what[0].second;
-            occurences[what[0]] += 1;
-        }
+        
+        count_occurences(content, occurences);
     }
     total_occurences_lock.lock();
     for (auto& p : occurences)
